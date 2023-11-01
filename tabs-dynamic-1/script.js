c20918f81534e9558c588e1c8b70fa2658be5054
alainfrk <119844905+alainfrk@users.noreply.github.com>
2023-11-01 13:22:17 +0100

    Using innerText where suited

--- tabs-dynamic-1/script.js
+++ tabs-dynamic-1/script.js
@@ -16,68 +16,68 @@
 
 // FUNCTION : fillTabsAndPanels
 // Note : in the JSON, "items" is defined as an array
-function fillTabsAndPanels (items){
-    items.forEach((element, index) =>{
-        const tab = document.getElementById(`item-tab-${index+1}`);
-        tab.innerHTML = element.tab;
+function fillTabsAndPanels(items) {
+  items.forEach((element, index) => {
+    const tab = document.getElementById(`item-tab-${index + 1}`);
+    tab.innerText = element.tab;
 
-        const panel = document.getElementById(`item-panel-${index+1}`);
-        panel.querySelector('h3').innerHTML = element.title;
-        panel.querySelector('p').innerHTML = element.article;
-    })
+    const panel = document.getElementById(`item-panel-${index + 1}`);
+    panel.querySelector("h3").innerText = element.title;
+    panel.querySelector("p").innerText = element.article;
+  });
 }
 
 // FUNCTION : assignPanelsAndTabsAndEventListeners
 // Note : as the layout is static, you know how many items will be shown in the DOM
-function assignPanelsAndTabsAndEventListeners(){
-    const totalItemsInDOM = 3;
+function assignPanelsAndTabsAndEventListeners() {
+  const totalItemsInDOM = 3;
 
-    for(let i = 1; i <= totalItemsInDOM; i++){
-        const targetedPanel = document.getElementById(`item-panel-${i}`);
-        const targetedTab = document.getElementById(`item-tab-${i}`);
+  for (let i = 1; i <= totalItemsInDOM; i++) {
+    const targetedPanel = document.getElementById(`item-panel-${i}`);
+    const targetedTab = document.getElementById(`item-tab-${i}`);
 
-        targetedTab.addEventListener('click', () => showPanelAndSelectTab(targetedPanel, targetedTab));
-    }
+    targetedTab.addEventListener("click", () => showPanelAndSelectTab(targetedPanel, targetedTab));
+  }
 }
 
 // FUNCTION : showPanelAndSelectTab
-const itemPanels = document.querySelectorAll('.item-panel');
-const itemTabs = document.querySelectorAll('.item-tab');
+const itemPanels = document.querySelectorAll(".item-panel");
+const itemTabs = document.querySelectorAll(".item-tab");
 
-function showPanelAndSelectTab(panelId, tabId){
-    itemPanels.forEach(element => {
-        element.style.display = 'none';
-    });
-    panelId.style.display = 'block';
-    
-    itemTabs.forEach(element => {
-        element.classList.remove('item-tab-selected')
-    });
-    tabId.classList.add('item-tab-selected');
+function showPanelAndSelectTab(panelId, tabId) {
+  itemPanels.forEach((element) => {
+    element.style.display = "none";
+  });
+  panelId.style.display = "block";
+
+  itemTabs.forEach((element) => {
+    element.classList.remove("item-tab-selected");
+  });
+  tabId.classList.add("item-tab-selected");
 }
 
 // DOMContentLoaded
 // Note : Added a specific error message for failed response
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('content.json')
-        .then(response => {
-            if(!response.ok){
-                throw new Error('Error at getting the response : ' +response.status);
-            }else{
-                return response.json();
-            }
-        })
-        .then(data => {
-            //console.log(data);
-            fillTabsAndPanels(data.items);
-            assignPanelsAndTabsAndEventListeners();
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("content.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error at getting the response : " + response.status);
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      //console.log(data);
+      fillTabsAndPanels(data.items);
+      assignPanelsAndTabsAndEventListeners();
 
-            // Show item 1 by default on page load
-            const itemPanel1 = document.getElementById('item-panel-1');
-            const itemTab1 = document.getElementById('item-tab-1');
-            showPanelAndSelectTab(itemPanel1, itemTab1);
-        })
-        .catch(error => {
-            console.log('DOMContentLoaded Error : ' +error)
-        })
-})
+      // Show item 1 by default on page load
+      const itemPanel1 = document.getElementById("item-panel-1");
+      const itemTab1 = document.getElementById("item-tab-1");
+      showPanelAndSelectTab(itemPanel1, itemTab1);
+    })
+    .catch((error) => {
+      console.log("DOMContentLoaded Error : " + error);
+    });
+});
--- tabs-dynamic-2/script.js
+++ tabs-dynamic-2/script.js
@@ -17,85 +17,84 @@
 
 // FUNCTION : createTabsAndPanels
 // Note : "items" is from the fetched JSON
-const tabsDynamic = document.getElementById('tabs-dynamic');
-const mainPanel = document.getElementById('main-panel');
+const tabsDynamic = document.getElementById("tabs-dynamic");
+const mainPanel = document.getElementById("main-panel");
 
-function createTabsAndPanels (items){
-    items.forEach((element, index) =>{
-        const createdDiv = document.createElement('div');
-        createdDiv.id = `item-tab-${index+1}`;
-        createdDiv.classList.add('item-tab');
-        createdDiv.innerHTML = element.tab;
-        tabsDynamic.appendChild(createdDiv);
+function createTabsAndPanels(items) {
+  items.forEach((element, index) => {
+    const createdDiv = document.createElement("div");
+    createdDiv.id = `item-tab-${index + 1}`;
+    createdDiv.classList.add("item-tab");
+    createdDiv.innerText = element.tab;
+    tabsDynamic.appendChild(createdDiv);
 
-        const createdArticle = document.createElement('article');
-        createdArticle.id = `item-panel-${index+1}`;
-        createdArticle.classList.add('item-panel');
+    const createdArticle = document.createElement("article");
+    createdArticle.id = `item-panel-${index + 1}`;
+    createdArticle.classList.add("item-panel");
 
-        const h3 = document.createElement('h3');
-        h3.textContent = element.title;
-        createdArticle.appendChild(h3);
+    const h3 = document.createElement("h3");
+    h3.innerText = element.title;
+    createdArticle.appendChild(h3);
 
-        const p = document.createElement('p');
-        p.textContent = element.article;
-        createdArticle.appendChild(p);
+    const p = document.createElement("p");
+    p.innerText = element.article;
+    createdArticle.appendChild(p);
 
-        mainPanel.appendChild(createdArticle)
-    })
+    mainPanel.appendChild(createdArticle);
+  });
 }
 
 // FUNCTION : assignPanelsAndTabsAndEventListeners
 // Note : numberOfItems is determined by the number of entries in the array "items" of the fetched json
-function assignPanelsAndTabsAndEventListeners(numberOfItems){
+function assignPanelsAndTabsAndEventListeners(numberOfItems) {
+  for (let i = 1; i <= numberOfItems; i++) {
+    const targetedPanel = document.getElementById(`item-panel-${i}`);
+    const targetedTab = document.getElementById(`item-tab-${i}`);
 
-    for(let i = 1; i <= numberOfItems; i++){
-        const targetedPanel = document.getElementById(`item-panel-${i}`);
-        const targetedTab = document.getElementById(`item-tab-${i}`);
-
-        targetedTab.addEventListener('click', () => showPanelAndSelectTab(targetedPanel, targetedTab));
-    }
+    targetedTab.addEventListener("click", () => showPanelAndSelectTab(targetedPanel, targetedTab));
+  }
 }
 
 // FUNCTION : showPanelAndSelectTab
-// IMPORTANT NOTE : const itemPanels and itemTabs moved inside the function 
+// IMPORTANT NOTE : const itemPanels and itemTabs moved inside the function
 // as the numbers of items could vary at runtime depending of your application
 // so it's updated each time the user click
-function showPanelAndSelectTab(panelId, tabId){
-    const itemPanels = document.querySelectorAll('.item-panel');
-    const itemTabs = document.querySelectorAll('.item-tab');
+function showPanelAndSelectTab(panelId, tabId) {
+  const itemPanels = document.querySelectorAll(".item-panel");
+  const itemTabs = document.querySelectorAll(".item-tab");
 
-    itemPanels.forEach(element => {
-        element.style.display = 'none';
-    });
-    panelId.style.display = 'block';
-    
-    itemTabs.forEach(element => {
-        element.classList.remove('item-tab-selected')
-    });
-    tabId.classList.add('item-tab-selected');
+  itemPanels.forEach((element) => {
+    element.style.display = "none";
+  });
+  panelId.style.display = "block";
+
+  itemTabs.forEach((element) => {
+    element.classList.remove("item-tab-selected");
+  });
+  tabId.classList.add("item-tab-selected");
 }
 
 // DOMContentLoaded
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('content.json')
-        .then(response => {
-            if(!response.ok){
-                throw new Error('Error at getting the response : ' +response.status);
-            }else{
-                return response.json();
-            }
-        })
-        .then(data => {
-            //console.log(data);
-            createTabsAndPanels(data.items);
-            assignPanelsAndTabsAndEventListeners((data.items).length);
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("content.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error at getting the response : " + response.status);
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      //console.log(data);
+      createTabsAndPanels(data.items);
+      assignPanelsAndTabsAndEventListeners(data.items.length);
 
-            // Show item 1 by default on page load
-            const itemPanel1 = document.getElementById('item-panel-1');
-            const itemTab1 = document.getElementById('item-tab-1');
-            showPanelAndSelectTab(itemPanel1, itemTab1);
-        })
-        .catch(error => {
-            console.log('DOMContentLoaded Error : ' +error)
-        })
-})
+      // Show item 1 by default on page load
+      const itemPanel1 = document.getElementById("item-panel-1");
+      const itemTab1 = document.getElementById("item-tab-1");
+      showPanelAndSelectTab(itemPanel1, itemTab1);
+    })
+    .catch((error) => {
+      console.log("DOMContentLoaded Error : " + error);
+    });
+});
